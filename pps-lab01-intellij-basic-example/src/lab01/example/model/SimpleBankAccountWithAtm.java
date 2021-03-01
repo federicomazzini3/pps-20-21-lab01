0f3dca0546e7b49e1d3679a1513ec5a728538ee9
Fred <federicomazzini.97@gmail.com>
2021-03-01 21:52:25 +0100

    refactor create AbstractSimpleBankAccount

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -1,32 +1,8 @@
 package lab01.example.model;
 
-public class SimpleBankAccountWithAtm implements BankAccount {
-
-    private double balance;
-    private final AccountHolder holder;
+public class SimpleBankAccountWithAtm extends AbstractSimpleBankAccount {
 
     public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
-        this.holder = holder;
-        this.balance = balance;
-    }
-
-    @Override
-    public AccountHolder getHolder() {
-        return null;
-    }
-
-    @Override
-    public double getBalance() {
-        return 0;
-    }
-
-    @Override
-    public void deposit(int usrID, double amount) {
-
-    }
-
-    @Override
-    public void withdraw(int usrID, double amount) {
-
+        super(balance, holder);
     }
 }
